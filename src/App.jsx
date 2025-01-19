bda1c0cf6166645d7ddd4ac21ade453b557e2f7a
Stivers-A <149453330+Stivers-A@users.noreply.github.com>
2025-01-18 16:23:19 -0800

    header of sample site created

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
-import 'bootstrap/dist/css/bootstrap.css';
-
+import "bootstrap/dist/css/bootstrap.css";
+import './App.css';
 function App() {
-
   return (
     <>
-      
-      <h1>Vite + React + Bootstrap</h1>
-      <nav>
-        <span className="text-primary">Brand Name</span>
-      </nav>
-      <p >
-        Sample Text
-      </p>
+      <div className="container-fluid px-0">
+        <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand " href="#">
+            <h1>Brand Name</h1>
+          </a>
+
+          <div className="navbar-nav px-5">
+                <a className="navbar-link px-4 link-underline-secondary" href="#">
+                  <h2>Homepage</h2>
+                </a>
+                <a className="navbar-link px-4 link-underline-secondary" href="#">
+                  <h2>View Products</h2>
+                </a>
+
+                <a className="navbar-link px-4 link-underline-secondary" href="#">
+                  <h2>Sign In</h2>
+                </a>
+
+                <a className="navbar-link px-4 link-underline-secondary" href="#">
+                  <h2>Cart</h2>
+                </a>
+          </div>
+        </nav>
+      </div>
+
+      <div>
+        <p>Sample Text</p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
