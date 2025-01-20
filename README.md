3c8646a0e5264982e909acd5acf46396efc8bd18
Stivers-A <149453330+Stivers-A@users.noreply.github.com>
2025-01-19 17:04:13 -0800

    made body section and now need to flesh it out

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # React + Bootstrap + Vite
 This site is a fascade of a modern E-Commerce site.
+# TODO
+1 put the div columns for the plans in a flex box
+2 put another div in the same flex to the right with a specific product advertisement
--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,55 @@
 import "bootstrap/dist/css/bootstrap.css";
-import './App.css';
+import "./App.css";
 function App() {
   return (
     <>
-      <div className="container-fluid px-0">
+    <div id="background">
+      <div id="pageHeader" className="container-fluid px-0">
         <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
-          <a className="navbar-brand " href="#">
+          <a className="navbar-brand px-2" href="#">
             <h1>Brand Name</h1>
           </a>
 
-          <div className="navbar-nav px-5">
-                <a className="navbar-link px-4 link-underline-secondary" href="#">
-                  <h2>Homepage</h2>
-                </a>
-                <a className="navbar-link px-4 link-underline-secondary" href="#">
-                  <h2>View Products</h2>
-                </a>
+          <div className="navbar-nav px-1">
+            <a
+              className="navbar-link px-4 link-underline-secondary link-offset-2"
+              href="#"
+            >
+              <h2>Homepage</h2>
+            </a>
+            <a
+              className="navbar-link px-4 link-underline-secondary link-offset-2"
+              href="#"
+            >
+              <h2>View Products</h2>
+            </a>
 
-                <a className="navbar-link px-4 link-underline-secondary" href="#">
-                  <h2>Sign In</h2>
-                </a>
+            <a
+              className="navbar-link px-4 link-underline-secondary link-offset-2"
+              href="#"
+            >
+              <h2>Sign In</h2>
+            </a>
 
-                <a className="navbar-link px-4 link-underline-secondary" href="#">
-                  <h2>Cart</h2>
-                </a>
+            <a
+              className="navbar-link px-4 link-underline-secondary link-offset-2"
+              href="#"
+            >
+              <h2>Cart</h2>
+            </a>
           </div>
         </nav>
       </div>
 
-      <div>
-        <p>Sample Text</p>
+      <div id="pageBody" className="my-2 p-5 container-fluid justify-content-start w-75">
+        <div class="row">
+          <div class="col-sm">One of three columns</div>
+          <div class="col-sm">Two of three columns</div>
+          <div class="col-sm">Three of three columns</div>
+        </div>
       </div>
+      <div id="pageFooter"></div>
+    </div>
     </>
   );
 }
